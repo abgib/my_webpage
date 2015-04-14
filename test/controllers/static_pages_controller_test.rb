c68b28f223997c7ab56c64edaee4fdeaa7e41aa7
stearagon <stearagon@gmail.com>
2015-04-14 16:00:38 -0400

    Finish static-pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Steve Aragon's Webpage"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Steve Aragon's Webpage"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Steve Aragon's Webpage"
   end
 
   test "should get projects" do
     get :projects
     assert_response :success
+    assert_select "title", "Projects | Steve Aragon's Webpage"
   end
 
 end
