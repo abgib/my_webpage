50dcddad3639fe27f81c53ec73b721384d98db50
stearagon <stearagon@gmail.com>
2015-04-14 16:20:06 -0400

    refactored Titles and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Steve Aragon's Webpage"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Steve Aragon's Webpage"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Steve Aragon's Webpage"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Steve Aragon's Webpage"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get projects" do
     get :projects
     assert_response :success
-    assert_select "title", "Projects | Steve Aragon's Webpage"
+    assert_select "title", "Projects | #{@base_title}"
   end
 
 end
